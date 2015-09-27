7bd8db1980df765f3dbd4a2540e1ffc61b070934
Ju Liu <ju.liu@welaika.com>
2015-09-27 01:26:43 +0100

    Add table formatting

--- issues/lib/issues/cli.ex
+++ issues/lib/issues/cli.ex
@@ -1,6 +1,12 @@
 defmodule Issues.CLI do
   @default_count 4
 
+  def run(argv) do
+    argv
+      |> parse_args
+      |> process
+  end
+
   def parse_args(argv) do
     parse = OptionParser.parse(argv, switches: [ help: :boolean ],
       aliases: [ h: :help ])
@@ -15,6 +21,83 @@ defmodule Issues.CLI do
       _ ->
         :help
     end
+  end
 
+  def process(:help) do
+    IO.puts """
+    usage: issues <user> <project> [ count | #{@default_count} ]
+    """
+    System.halt(0)
+  end
+
+  def process({user, project, count}) do
+    Issues.GithubIssues.fetch(user, project)
+      |> decode_response
+      |> convert_to_list_of_hashdicts
+      |> sort_into_ascending_order
+      |> Enum.take(count)
+      |> format_into_table
+  end
+
+  def decode_response({:ok, body}), do: body
+  def decode_response({:error, error}) do
+    {_, message} = List.keyfind(error, "message", 0)
+    IO.puts "Error fetching from Github: #{message}"
+    System.halt(2)
+  end
+
+  def convert_to_list_of_hashdicts(list) do
+    list
+      |> Enum.map(&Enum.into(&1, HashDict.new))
+  end
+
+  def sort_into_ascending_order(list) do
+    list
+      |> Enum.sort(&(&1["created_at"] < &2["created_at"]))
+  end
+
+  def format_into_table(list) do
+    data = [header | extract_data(list)]
+
+    max_lens = Enum.map(transpose(data), &get_max_string_length/1)
+
+    [ fitted_header | rows ] = data
+      |> Enum.map(fn row ->
+        row
+        Enum.zip(row, max_lens)
+          |> Enum.map(fn({value, len}) ->
+            " #{String.ljust(value, len, ?\s)} "
+          end)
+          |> Enum.join("|")
+      end)
+
+    separator = max_lens
+      |> Enum.map(fn len -> String.duplicate("-", len + 2) end)
+      |> Enum.join("+")
+
+    [ fitted_header, separator | rows ]
+      |> Enum.map(&(&1 <> "\n"))
+      |> Enum.join
+  end
+
+  def header do
+    ["#", "created_at", "title"]
+  end
+
+  def extract_data(list) do
+    for element <- list do
+      [element["id"], element["created_at"], element["title"]] |> Enum.map(&to_string/1)
+    end
+  end
+
+  def transpose([[]|_]), do: []
+  def transpose(list) do
+    [Enum.map(list, &hd/1) | transpose(Enum.map(list, &tl/1))]
+  end
+
+  def get_max_string_length(list) do
+    list
+      |> Enum.map(&(String.length(to_string(&1))))
+      |> Enum.max
   end
 end
--- issues/test/issues_test.exs
+++ issues/test/issues_test.exs
@@ -1,7 +1,12 @@
 defmodule IssuesTest do
   use ExUnit.Case
 
-  import Issues.CLI, only: [ parse_args: 1 ]
+  import Issues.CLI, only: [
+    parse_args: 1,
+    sort_into_ascending_order: 1,
+    convert_to_list_of_hashdicts: 1,
+    format_into_table: 1
+  ]
 
   test "returns :help when -h or --help options are passed" do
     assert parse_args(["-h", "anything"]) == :help
@@ -14,5 +19,35 @@ defmodule IssuesTest do
 
   test "returns two values and default count if two are passed" do
     assert parse_args(["user", "project"]) == { "user", "project", 4 }
+  end
+
+  test "sorts the issues by ascending creation date" do
+    data = fake_list([2, 1, 3])
+
+    assert Enum.map(sort_into_ascending_order(data), &(&1["created_at"])) == [ 1, 2, 3 ]
+  end
+
+  test "formats the data nicely into a table" do
+    data = fake_list([1, 20, 300])
+
+    assert format_into_table(data) == """
+     #   | created_at | title           
+    -----+------------+-----------------
+     1   | 1          | hello there 1   
+     20  | 20         | hello there 20  
+     300 | 300        | hello there 300 
+    """
+  end
+
+  defp fake_list(values) do
+   data = for value <- values do
+     [
+       {"id", value},
+       {"created_at", value},
+       {"title", "hello there #{value}"}
+     ]
+   end
+
+   convert_to_list_of_hashdicts(data)
   end
 end
