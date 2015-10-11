b61a5f4ed223ece4f20f120f2d4c9808395a21ac
Ju Liu <ju.liu@welaika.com>
2015-10-12 00:22:08 +0100

    Rewrite sequence server to use supervision tree

---
+++ otp/sequence_sup/lib/sequence_sup/stash.ex
@@ -0,0 +1,27 @@
+defmodule SequenceSup.Stash do
+  use GenServer
+
+  # public API
+
+  def start_link(initial) do
+    {:ok, pid} = GenServer.start_link(__MODULE__, initial)
+  end
+
+  def get_value(stash_pid) do
+    GenServer.call(stash_pid, :get_value)
+  end
+
+  def save_value(stash_pid, value) do
+    GenServer.cast(stash_pid, {:save_value, value})
+  end
+
+  # GenServer implementation
+
+  def handle_call(:get_value, _from, current_number) do
+    {:reply, current_number, current_number}
+  end
+
+  def handle_cast({:save_value, value}, _current_number) do
+    {:noreply, value}
+  end
+end
---
+++ otp/sequence_sup/lib/sequence_sup/supervisor.ex
@@ -0,0 +1,18 @@
+defmodule SequenceSup.Supervisor do
+  use Supervisor
+
+  def start_link(initial) do
+    result = {:ok, sup} = Supervisor.start_link(__MODULE__, [initial])
+    start_workers(sup, initial)
+    result
+  end
+
+  def start_workers(sup, initial) do
+    {:ok, stash} = Supervisor.start_child(sup, worker(SequenceSup.Stash, [initial]))
+    Supervisor.start_child(sup, supervisor(SequenceSup.SubSupervisor, [stash]))
+  end
+
+  def init(_) do
+    supervise [], strategy: :one_for_one
+  end
+end
