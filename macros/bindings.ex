9024cf6acb6ed524718471ee83e62497838a093e
Ju Liu <ju.liu@welaika.com>
2015-10-13 18:37:35 +0100

    More macros fun

---
+++ macros/bindings.ex
@@ -0,0 +1,18 @@
+defmodule My do
+  defmacro mydef(name) do
+    quote bind_quoted: [name: name] do
+      def unquote(name)(), do: unquote(name)
+    end
+  end
+end
+
+defmodule Test do
+  require My
+
+  My.mydef(:hello)
+
+  [:fred, :bert] |> Enum.each(&My.mydef(&1))
+end
+
+IO.puts Test.hello
+IO.puts Test.fred
